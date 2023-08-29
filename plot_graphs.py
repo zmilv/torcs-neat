55c437f44d7f8cbfa470373b8eb40c803aaf73ca
zygis <z.milvydas@gmail.com>
2023-08-29 22:53:06 +0300

    add lap time scatter plot

--- plot_graphs.py
+++ plot_graphs.py
@@ -1,6 +1,6 @@
 import ast
-
 from matplotlib import pyplot as plt
+
 
 # Extract results from the text file
 with open("results.txt", "r") as file:
@@ -11,16 +11,22 @@ for i, line in enumerate(lines):
         cars = ast.literal_eval(lines[i + 1].strip())
     elif line == "Fitnesses:":
         fitnesses = ast.literal_eval(lines[i + 1].strip())
+    elif line == "Laptimes:":
+        laptimes = ast.literal_eval(lines[i + 1].strip())
 
-# Parse results to a list of generations and a dictionary mapping each generation to its results
+
+# Parse results to a list of generations and a dictionaries mapping each generation to its fitness results and lap times
 generations = []
 result_dict = {}
+laptimes_dict = {}
 for i, item in enumerate(cars):
-    generation = item.split("C")[0][1:]
+    generation = int(item.split("C")[0][1:])
     if generation not in generations:
         generations.append(generation)
         result_dict[generation] = []
+        laptimes_dict[generation] = []
     result_dict[generation].append(fitnesses[i])
+    laptimes_dict[generation].append(laptimes[i])
 
 # Calculate the maximum result and average of top 5 results from each generation
 max_results = []
@@ -32,11 +38,38 @@ for key, results in result_dict.items():
     top5_average = sum(top5_results) / len(top5_results)
     top5_averages.append(top5_average)
 
+# Get data needed for the lap time plot
+laptime_generations = generations
+best_laptimes = []
+for key, results in laptimes_dict.items():
+    generation_laptimes = [x for x in results if x != 0.0]
+    if not generation_laptimes:
+        laptime_generations = [x for x in laptime_generations if x != key]
+    else:
+        best_laptimes.append(min(generation_laptimes))
 
-# Plot graph
-plt.plot(generations, max_results, label="Maximum")
-plt.plot(generations, top5_averages, label="Top 5 average")
-plt.xlabel("Generations")
-plt.ylabel("Fitness")
-plt.legend()
+
+# Plot graphs
+fig, ax1 = plt.subplots()
+
+ax1.plot(generations, max_results, label="Maximum Fitness", color="dodgerblue")
+ax1.plot(generations, top5_averages, label="Top 5 average Fitness", color="orange")
+ax1.set_xlabel("Generations")
+ax1.set_ylabel("Fitness")
+
+ax2 = ax1.twinx()
+ax2.scatter(
+    laptime_generations,
+    best_laptimes,
+    label="Best Lap time",
+    color="mediumseagreen",
+    marker=".",
+)
+ax2.set_ylabel("Seconds")
+
+lines, labels = ax1.get_legend_handles_labels()
+lines2, labels2 = ax2.get_legend_handles_labels()
+ax1.legend(lines + lines2, labels + labels2)
+
+plt.title("Fitness and Lap time evolution ")
 plt.show()
